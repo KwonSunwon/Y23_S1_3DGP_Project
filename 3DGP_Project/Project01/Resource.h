a8b99804fdabea405fdd9dd149219dd66c00a30a
KwonSunwon <kso00011147@gmail.com>
2023-04-22 14:19:52 +0900

    Project sln 다시 생성

--- 3DGP_Project/Project01/Resource.h
+++ 3DGP_Project/Project01/Resource.h
@@ -1,17 +1,17 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// 다음에서 사용 Proj01_Tank_Battle.rc
+// 다음에서 사용 LabProject02-02.rc
 
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_PROJ01TANKBATTLE_DIALOG	102
+#define IDD_LABPROJECT0202_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_PROJ01TANKBATTLE			107
+#define IDI_LABPROJECT0202			107
 #define IDI_SMALL				108
-#define IDC_PROJ01TANKBATTLE			109
+#define IDC_LABPROJECT0202			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
